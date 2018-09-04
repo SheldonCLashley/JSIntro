9adc0bc93ba4ba1df391bd340e925ba9f5562a96
Sheldon Lashley <sheldonclashley@gmail.com>
2018-09-04 15:49:05 -0500

    Minimalist Calculator

--- Desktop/app.js
+++ Desktop/app.js
@@ -1,3 +1,20 @@
 //3..2..1 Code Age Display
+console.log("Presentation");
 console.log("Sheldon");
 console.log(20);
+console.log();
+//3..2..1 Minimalistic Calculator
+var num1 = 6;
+var num2 = 3;
+
+console.log("Minimalistic Calculator");
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log();
+
+
+
+
+console.log();
